4cbb958ade3c702898df73c6d8d1505fc772fda4
dravencirelia <2594262717@qq.com>
2025-03-02 15:51:23 +0800

    add EulerTour

---
+++ Tree/EulerTour.py
@@ -0,0 +1,12 @@
+# 欧拉遍历
+#
+#
+from LinkedBinaryTree import LinkedBinaryTree
+
+
+class EnlerTour(LinkedBinaryTree):
+    def execute(self):
+        pass
+
+    def _tour(self, p, d, path):
+        pass
